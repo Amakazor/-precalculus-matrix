6ed150f82c1adb165a5c8ce7bc94f481979feb88
amakazor <amakazor@gmail.com>
2020-03-11 18:46:30 +0100

    LoggingInterface and additional constructors in Matrix

--- matrix-app/Program.cs
+++ matrix-app/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Amakazor.Matrix;
+using Amakazor;
 
 namespace matrix_app
 {
@@ -11,7 +11,7 @@ namespace matrix_app
     {
         static void Main(string[] args)
         {
-            Matrix m1 = new Matrix(4, 6);
+            Matrix m1 = new Matrix(4, 6, true);
             m1.RandomizeMatrix(-10, 10);
             m1.Display();
 
--- matrix-app/matrix.cs
+++ matrix-app/matrix.cs
@@ -3,22 +3,60 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Amakazor;
 
-namespace Amakazor.Matrix
+namespace Amakazor
 {
-    class Matrix
+    class Matrix : LoggingInterface
     {
         private List<List<long>> matrixList;
+        private bool enableLogging;
         private int LongestElement;
 
         public Matrix()
         {
-            matrixList = new List<List<long>>();
-            LongestElement = 0;
+            InitializeEmpty();
+        }
+        
+        public Matrix(bool Logging)
+        {
+            EnableLogging = Logging;
+            InitializeEmpty();
         }
 
         public Matrix(int size)
         {
+            InitializeSquare(size);
+        }
+        
+        public Matrix(int size, bool Logging)
+        {
+            EnableLogging = Logging;
+            InitializeSquare(size);
+        }
+
+        public Matrix(int rows, int columns)
+        {
+            InitializeRect(rows, columns);
+        }
+        
+        public Matrix(int rows, int columns, bool Logging)
+        {
+            EnableLogging = Logging;
+            InitializeRect(rows, columns);
+        }
+
+        private void InitializeEmpty()
+        {
+            Log("Constructing empty matrix...");
+            matrixList = new List<List<long>>();
+            LongestElement = 0;
+            Log("Empty matrix constructed.");
+        }
+
+        private void InitializeSquare(int size)
+        {
+            Log("Constructing matrix...");
             matrixList = new List<List<long>>(size);
             for (int i = 0; i < size; i++)
             {
@@ -31,10 +69,12 @@ namespace Amakazor.Matrix
             }
 
             this.LongestElement = CalculateLongestElement();
+            Log("Matrix constructed.");
         }
 
-        public Matrix(int rows, int columns)
+        private void InitializeRect(int rows, int columns)
         {
+            Log("Constructing matrix...");
             matrixList = new List<List<long>>(rows);
             for (int i = 0; i < rows; i++)
             {
@@ -47,10 +87,25 @@ namespace Amakazor.Matrix
             }
 
             this.LongestElement = CalculateLongestElement();
+            Log("Matrix constructed.");
+        }
+
+        public bool EnableLogging
+        {
+            get
+            {
+                return enableLogging;
+            }
+
+            set
+            {
+                enableLogging = value;
+            }
         }
 
         public void Display()
         {
+            Log("Displaying matrix...");
             if (matrixList.Count > 0)
             {
                 Console.Write('\u250C');
@@ -87,6 +142,7 @@ namespace Amakazor.Matrix
                 Console.Write('\u2518');
                 Console.Write('\n');
             }
+            Log("Matrix was displayed.");
         }
 
         public int GetNumberOfColumns()
@@ -101,8 +157,21 @@ namespace Amakazor.Matrix
             }
         }
 
+        public void Log(string message)
+        {
+            if (EnableLogging)
+            {
+                ConsoleColor CurrentColor = Console.ForegroundColor;
+
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(message);
+                Console.ForegroundColor = CurrentColor;
+            }
+        }
+
         public void RandomizeMatrix(int minValue, int maxValue)
         {
+            Log("Randomizing matrix...");
             Random random = new Random();
 
             for (int i = 0; i < matrixList.Count; i++)
@@ -114,6 +183,7 @@ namespace Amakazor.Matrix
             }
 
             this.LongestElement = CalculateLongestElement();
+            Log("Matrix randomized.");
         }
 
         private int CalculateLongestElement()
