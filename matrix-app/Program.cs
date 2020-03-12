e1dbbc748375ac0f0bdbc16c1c581a3dfb55da46
amakazor <amakazor@gmail.com>
2020-03-13 00:52:21 +0100

    Additional tests

--- matrix-app/Program.cs
+++ matrix-app/Program.cs
@@ -19,6 +19,17 @@ namespace matrix_app
             m2.Copy(m1);
             m2.Display();
 
+            Matrix m3 = new Matrix(3, true);
+            m3.RandomizeMatrix(-10, 10);
+            m3.Display();
+
+            Matrix m4 = new Matrix(m3, true);
+            m4.Display();
+            m4.RandomizeMatrix(-10, 10);
+
+            m3.Display();
+            m4.Display();
+
 
             Console.ReadKey();
         }
