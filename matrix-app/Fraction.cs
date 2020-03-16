fab389b6231fc368558a1c512fddabb32f39fd56
amakazor <amakazor@gmail.com>
2020-03-16 22:22:52 +0100

    Fraction Class

---
+++ matrix-app/Fraction.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Amakazor
+{
+    class Fraction : ILoggingInterface
+    {
+        private long Numerator;
+        private long Denominator;
+
+        public bool EnableLogging { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public void Log(string message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
